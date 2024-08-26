b751bd14bf40a3bae9bd3c0df4aaf7ecb31a592d
Rouven Hi! <3582050+RouHim@users.noreply.github.com>
2024-08-26 21:37:20 +0200

    Fix: ci pipe

--- .github/workflows/test-image.sh
+++ .github/workflows/test-image.sh
@@ -2,13 +2,13 @@
 
 # Spin up a new container
 echo "🚀 Spinning up a test container"
-docker-compose up -d
+docker compose up -d
 
 # Loop until the string is found
 echo "🔍 Checking for the desired string in the logs..."
 while true; do
   # Check if the desired string is in the logs
-  if docker-compose logs | grep -q "Please restart the server"; then
+  if docker compose logs | grep -q "Please restart the server"; then
     echo "✅ Dedicated server started successfully"
     break
   fi
@@ -16,7 +16,7 @@ while true; do
   # Print the 5 lines tail of the logs
   echo "📃 Desired string not found in the logs, printing the last 5 lines of the logs:"
   echo "========================================"
-  docker-compose logs --tail 5
+  docker compose logs --tail 5
   echo "========================================"
 
   # Wait for 5 seconds before checking again
@@ -25,6 +25,6 @@ while true; do
 done
 
 # Cleanup and exit with 0
-docker-compose kill && docker-compose down --volumes
+docker compose kill && docker compose down --volumes
 echo "✅ Done, everything looks good"
 exit 0
